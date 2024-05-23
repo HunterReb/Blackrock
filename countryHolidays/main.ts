cb1691f52244c279bed232d6ce79ce845fc62b8e
Rebecca Hunter <rh.hunterrebecca@gmail.com>
2024-05-23 22:43:43 +0100

    Should be functional, just have to check different methods of having the GET work as intended within instructions from assessment file

--- countryHolidays/main.ts
+++ countryHolidays/main.ts
@@ -1,4 +1,4 @@
-import * as URL from 'url'
+import * as URL from 'url' //Fairly sure I need to install a module into this for the GET to work from API, spend some time looking into options
 
 interface Holiday {
     name: string;
@@ -10,20 +10,20 @@ interface Holiday {
 async function getHolidays() {
     const response = await fetch('https://date.nager.at/api/publicholidays/2024/Austria');
     const holidays = await response.json();
-    const csvInformation: Holiday.[] = [];
+    const csvData: Holiday[] = [];
 
     for (const holiday of holidays) {
         const date = new Date(holiday.date);
-        const daysUntil = date > new Date() ? `+${Math.round((date.getTime() - Date.now()) / (1000 * 86400))}` : '';
+        const daysUntil = date > new Date() ? `+${Math.round((date.getTime() - Date.now()) / (1000 * 86400))}` : ''; 
         const weekends = holiday.date.includes('Saturday') || holiday.date.includes('Sunday') ? 'Yes' : 'No';
         csvData.push({ //Went through whole file, forgot that csv parser module is needed for this
-            Name: holiday.name,
-            Date: holiday.date,
-            Daysuntil: daysUntil, //Figure out the spacing issue here, only allows one word
-            Weekends: weekends,
+            name: holiday.name,
+            date: holiday.date,
+            daysUntil: daysUntil, 
+            weekends: weekends,
         });
     }
-}
+
 
 const csvString = csvData.map((holiday) => Object.values(holiday).join(',')).join('/n');
 const csvFile = `austrian-holidays-2024.csv`;
@@ -37,8 +37,11 @@ await
 fetch('https://docs.google.com/forms/d/e/1FAIP2AaM9U3KUqOxQYwXsQjYkZ7v__KQxGwLQK3iO1vGjPkgRqZMzQ/formResponse',
 {
     method: 'POST',
-    body: formData
+    body: formData,
 });
+
+console.log(`CSV file saved to ${csvFile}`) //Providing feedback to confirm the necessary action is completed
+}
 
 getHolidays();
 
